1ed1aa6e2359ff2dfb15ff392cea29711243c5a1
yutaoz <47333239+yutaoz@users.noreply.github.com>
2021-01-04 22:42:27 -0500

    Updated send error checking to check if both users tagged are initilized

--- Commands/send.js
+++ Commands/send.js
@@ -6,22 +6,25 @@ module.exports = {
     description: "Send schlockcoin to user",
     execute(msg, args) {
 
-        var initCheckResult;
-
         const client = new MongoClient(URI);
 
-        async function initCheck(id) {
-            let found = false;
+        async function initCheck(id, mention) { // check if both users are initialized
+            let found1 = false;
+            let found2 = false;
             try {
                 await client.connect();
 
                 var db = client.db("digicurr");
 
                 if (await db.collection("accounts").countDocuments({accountName: id}) !== 0) { //check if user exists
-                    found = true;
+                    found1 = true;
                 }
 
-                return found;
+                if (await db.collection("accounts").countDocuments({accountName: mention}) !== 0) { // check if tagged user exists
+                    found2 = true;
+                }
+
+                return [found1, found2];
 
             } catch (e) {
                 console.error(e);
@@ -31,18 +34,22 @@ module.exports = {
             }
         }
 
-        initCheck(msg.author.id).then((response) => {
-            initCheckResult = response;
+        // do error check with initCheck results
+        initCheck(msg.author.id, msg.mentions.users.first().id).then((response) => {
+            errorCheck(response);
         })
 
         function errorCheck(response) {
             if ((args.length !== 2) || (msg.mentions.users.size !== 1)) { // check command syntax
                 msg.reply("Error: Expected format '>send <amount> <tagged user>'");
-            } else if (!initCheckResult) {
-                console.log(initCheckResult);
+            } else if (args[1] !== `<@!${msg.mentions.users.first().id}>`) {
+                msg.reply("Error: Expected format '>send <amount> <tagged user>'");
+            } else if (msg.author.id === msg.mentions.users.first().id) {
+                msg.reply("Sending money to yourself.... sad");
+            } else if (!response[0]) {
                 msg.reply("Initialize account first with >init");
-            } else if (typeof args[0] === 'number') {
-                msg.reply("3");
+            } else if (!response[1]) {
+                msg.reply("Tagged user must initialize account first with >init");
             } else{
                 msg.reply("success");
             }
